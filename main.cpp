a939d3399edc8d24dc131991b130cf2ac223b8f4
SarahKoziol <sarah.koziol1991@gmail.com>
2017-06-19 00:26:24 +0200

    read folder instead of single file, using dirent.h

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "lib/tinyxml2/tinyxml2.h"
+#include "lib/dirent/dirent.h" // TODO include only on windows
 
 /*
  * Author: Sarah Koziol
@@ -11,16 +12,35 @@ using namespace std;
 using namespace tinyxml2;
 
 int main() {
-    std::string fileToRead;
-    std::cout << "Pfad zur Datei:" << std::endl;
-    std::getline (cin, fileToRead);
-    std::cout << "Eingegebner Pfad: " << fileToRead <<std::endl;
+    std::string dirToRead;
+    std::cout << "Pfad zum Ordner:" << std::endl;
+    std::getline (cin, dirToRead);
+//    std::cout << "Eingegebner Pfad: " << dirToRead <<std::endl;
 
-    XMLDocument xmlDocument;
-    xmlDocument.LoadFile( fileToRead.c_str() );
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (dirToRead.c_str())) != NULL) {
+        /* print all the files and directories within directory */
+        while ((ent = readdir (dir)) != NULL) {
+            if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..") || !strcmp(ent->d_name, "?")) { // TODO fix ? for filenames with strange chars, like "B0144 Schr÷der.xml"
+                // do nothing of ".", ".." or "?"
+            } else {
+                std::cout << "File: " << ent->d_name << std::endl;
 
-    const char* keywords = xmlDocument.FirstChildElement("fields")->FirstChildElement("Keywords")->GetText();
+                XMLDocument xmlDocument;
+                xmlDocument.LoadFile( (dirToRead + "\\" + ent->d_name).c_str() );
 
-    std::cout << keywords << std::endl;
+                const char* keywords = xmlDocument.FirstChildElement("fields")->FirstChildElement("Keywords")->GetText();
+
+                std::cout << keywords << std::endl;
+            }
+        }
+        closedir (dir);
+    } else {
+        /* could not open directory */
+        perror ("");
+        return EXIT_FAILURE;
+    }
+
     return 0;
 }
