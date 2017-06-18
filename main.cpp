2a260c588dd0082a7538261750f2cf042838bf4b
SarahKoziol <sarah.koziol1991@gmail.com>
2017-06-18 22:22:54 +0200

    first version working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "lib/tinyxml2/tinyxml2.h"
+
+/*
+ * Author: Sarah Koziol
+ *
+ * Auswertung von PDF-Formularen
+ */
+
+using namespace std;
+using namespace tinyxml2;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string fileToRead;
+    std::cout << "Pfad zur Datei:" << std::endl;
+    std::getline (cin, fileToRead);
+    std::cout << "Eingegebner Pfad: " << fileToRead <<std::endl;
+
+    XMLDocument xmlDocument;
+    xmlDocument.LoadFile( fileToRead.c_str() );
+
+    const char* keywords = xmlDocument.FirstChildElement("fields")->FirstChildElement("Keywords")->GetText();
+
+    std::cout << keywords << std::endl;
     return 0;
 }
