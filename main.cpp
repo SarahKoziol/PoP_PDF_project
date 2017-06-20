c394fd83313d12a69effba54bb0910280fc1acfc
SarahKoziol <sarah.koziol1991@gmail.com>
2017-06-21 01:24:57 +0200

    vector to select used fields; write file with output

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include "lib/tinyxml2/tinyxml2.h"
 #include "lib/dirent/dirent.h" // TODO include only on windows
 
@@ -12,6 +14,15 @@ using namespace std;
 using namespace tinyxml2;
 
 int main() {
+    // Configure here which fields (=> xml-nodes) to output, like `"Keywords"` for `<Keywords />`
+    const std::vector<std::string> fieldsToOutput = {"Vorname",
+                                                     "Nachname",
+                                                     "Titel",
+                                                     "Semester",
+                                                     "Kurzfassung",
+                                                     "Schlagwoerter"}; // TODO create external option
+    const std::string outputFormat = "HTML"; // TODO create external option AND switch down below for different filetypes
+
     std::string dirToRead;
     std::cout << "Pfad zum Ordner:" << std::endl;
     std::getline (cin, dirToRead);
@@ -20,22 +31,42 @@ int main() {
     DIR *dir;
     struct dirent *ent;
     if ((dir = opendir (dirToRead.c_str())) != NULL) {
-        /* print all the files and directories within directory */
+        ofstream outputFile;
+        outputFile.open("outputs.rtf"); // TODO path; default path; path option
+
+        bool first = true;
+
         while ((ent = readdir (dir)) != NULL) {
+            // TODO check if XML
             if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..") || !strcmp(ent->d_name, "?")) { // TODO fix ? for filenames with strange chars, like "B0144 Schr÷der.xml"
                 // do nothing of ".", ".." or "?"
             } else {
+                if (!first) {
+                    outputFile << std::endl << std::endl;
+                } else {
+                    first = false;
+                }
+
                 std::cout << "File: " << ent->d_name << std::endl;
 
                 XMLDocument xmlDocument;
                 xmlDocument.LoadFile( (dirToRead + "\\" + ent->d_name).c_str() );
+                const XMLElement* fields = xmlDocument.FirstChildElement("fields"); // TODO check if fields exist
 
-                const char* keywords = xmlDocument.FirstChildElement("fields")->FirstChildElement("Keywords")->GetText();
-
-                std::cout << keywords << std::endl;
+                for (auto value : fieldsToOutput) {
+                    if (fields->FirstChildElement(value.c_str())->GetText() != nullptr) {
+                        const char* currentField = fields->FirstChildElement(value.c_str())->GetText();
+                        std::cout << value << ": " << currentField << std::endl;
+                        outputFile << value << ": " << currentField << std::endl;
+                    } else {
+                        std::cout << value << ": " << std::endl;
+                        outputFile << value << ": " << std::endl;
+                    }
+                }
             }
         }
         closedir (dir);
+        outputFile.close();
     } else {
         /* could not open directory */
         perror ("");
